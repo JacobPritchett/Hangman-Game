614802d4f263c163b9560d456cf4d9bd4d01d0d0
Jacob Pritchett <baconramen@gmail.com>
2017-11-11 01:04:50 -0700

    more changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,9 @@
 // Creates an array with all possible words
-var wordChoices =
-["calamity","cataclysm","catastrophe","disaster","downfall","annihilation";"condemnation","death","sentence","tragedy"];
+var wordChoices = ["calamity", "cataclysm", "catastrophe", "disaster", "downfall", "annihilation",
+    "condemnation", "death", "sentence", "tragedy"];
 
 // Creates an array that holds the alphabet
-var alphabet =
-["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Creates variables to keep track of wins and losses
 var wins = 0;
@@ -13,31 +12,48 @@ var losses = 0;
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
 
-	// Determines which key was pressed.
-	var userGuess = event.key;
+    // Determines which key was pressed.
+    var userGuess = event.key;
 
-	// Randomly picks a word from our array
-//	var wordPick = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+    // Randomly picks a word from our array
+    	var wordPick = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 
-	function wordPick () {
-		return wordChoices[Math.floor(Math.random() * wordChoices.length)];
-	}
+    //function wordPick() {
+    //    return wordChoices[Math.floor(Math.random() * wordChoices.length)];
+    //}
 }
 
 
 
+function resetUI () {
+    $('.guessed-letter').remove();
+    $('.shown-letter').remove();
+}
 
-function resetGame () {
+
+
+function resetGame() {
     resetUI();
-    gameAnswer = wordPick();
+    gameAnswer = wordPick;
     gameShownAnswer = blanksFromAnswer(gameAnswer);
     hangmanState = 0;
-    drawWord(gameShownAnswer);    
+    drawWord(gameShownAnswer);
 }
+
 $(document).ready(resetGame);
-function win () { wins++ ;}
-function lose () { losses++ ; }
-function doKeypress () {
+
+function win() { wins++; }
+
+function lose() { losses++; }
+
+function doKeypress() {
     var tempChar = $('#letter-input').val().toLowerCase();
     var tempString = "";
     $('#letter-input').val("");
+
+    var htmlWins =
+        "<p>Wins " + wins + "</p>" +
+        "<p>Losses " + losses + "</p>";
+
+    document.querySelector("#scoreboard").innerHTML = htmlWins;
+}
